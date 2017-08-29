f13f8de35260e1f5556b75f2ceb899d3faf89fb5
emmanouil e <emmanouil.e25@gmail.com>
2017-08-29 16:18:53 +0100

    add publisher entity, publisher repository, and modify book entity to add the one to one relationship between book and publisher and also modify the DevBootstrap class to save some publisher data into the embedded database

--- spring5webapp/src/main/java/com/manos/spring5webapp/bootstrap/DevBootstrap.java
+++ spring5webapp/src/main/java/com/manos/spring5webapp/bootstrap/DevBootstrap.java
@@ -2,8 +2,10 @@ package com.manos.spring5webapp.bootstrap;
 
 import com.manos.spring5webapp.model.Author;
 import com.manos.spring5webapp.model.Book;
+import com.manos.spring5webapp.model.Publisher;
 import com.manos.spring5webapp.repositories.AuthorRepository;
 import com.manos.spring5webapp.repositories.BookRepository;
+import com.manos.spring5webapp.repositories.PublisherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
@@ -19,6 +21,9 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
     @Autowired
     private BookRepository bookRepository;
 
+    @Autowired
+    private PublisherRepository publisherRepository;
+
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
         initData();
@@ -28,7 +33,10 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
         //Eric
         Author eric = new Author("Eric", "Evans");
-        Book  ddd = new Book("Domain Driven Design", "1234", "Harper Collins!!");
+        Publisher harperCollins = new Publisher("Harper Collins!!","address1");
+        publisherRepository.save(harperCollins);
+
+        Book  ddd = new Book("Domain Driven Design", "1234", harperCollins);
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
 
@@ -38,7 +46,9 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
         //Rod
         Author rod = new Author("Rod", "Johnson");
-        Book noEJB = new Book("J2EE Development without EJB", "23444", "Wrox" );
+        Publisher wrox = new Publisher("Wrox","address2");
+        publisherRepository.save(wrox);
+        Book noEJB = new Book("J2EE Development without EJB", "23444", wrox);
         rod.getBooks().add(noEJB);
 
         authorRepository.save(rod);
---
+++ spring5webapp/src/main/java/com/manos/spring5webapp/repositories/PublisherRepository.java
@@ -0,0 +1,7 @@
+package com.manos.spring5webapp.repositories;
+
+import com.manos.spring5webapp.model.Publisher;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PublisherRepository extends CrudRepository<Publisher,Long> {
+}
